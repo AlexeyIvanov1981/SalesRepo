1696f4e26b9471b7864d28debbba4713680e6615
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2023-01-16 13:03:11 +0300

    added maven

---
+++ src/test/java/SalesTestClass.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class SalesTestClass {
+
+    SalesManager sales = new SalesManager(new long[]{1, 20, 50, 90, 110});
+
+    @Test
+    public void maxSilesTest(){
+
+    }
+}
